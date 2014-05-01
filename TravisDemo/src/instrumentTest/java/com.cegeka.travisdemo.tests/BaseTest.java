d166f1f607266118077d977812f771c737f05421
irina <irina2045@yahoo.com>
2014-05-01 08:09:59 +0300

    Initial commit

---
+++ TravisDemo/src/instrumentTest/java/com.cegeka.travisdemo.tests/BaseTest.java
@@ -0,0 +1,15 @@
+package com.cegeka.travisdemo.tests;
+
+import android.test.AndroidTestCase;
+
+public class BaseTest extends AndroidTestCase{
+
+    public void testPassingCondition(){
+        assertEquals(true, true);
+    }
+
+    public void testFailingCondition(){
+        assertEquals(true, false);
+    }
+
+}
